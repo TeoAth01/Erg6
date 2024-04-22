dc01cf72ee9b72e67a0fdc3a88e05b9ab022f39a
TeoAth01 <82370290+TeoAth01@users.noreply.github.com>
2024-04-22 17:00:43 +0300

    Non parallel code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,57 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        String myFileName = new String("E.coli.txt");
+        String myPattern = new String("tacccagattatcgccatcaacgg");
+
+        String fileString = null;//, StandardCharsets.UTF_8);
+        try {
+            fileString = new String(Files.readAllBytes(Paths.get(myFileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        char[] text = new char[fileString.length()];
+        int n = fileString.length();
+
+        for (int i = 0; i < n; i++) {
+            text[i] = fileString.charAt(i);
+        }
+
+        String patternString = new String(myPattern);
+        char[] pattern = new char[patternString.length()];
+        int m = patternString.length();
+        for (int i = 0; i < m; i++) {
+            pattern[i] = patternString.charAt(i);
+        }
+
+        int matchLength = n - m;
+        char[] match = new char[matchLength+1];
+        for (int i = 0; i <= matchLength; i++) {
+            match[i] = '0';
+        }
+        /*ArrayList<Integer> match = new ArrayList<Integer>(); */
+        int matchCount = 0;
+        for (int j = 0; j < matchLength; j++) {
+            int i;
+            for (i = 0; i < m && pattern[i] == text[i + j]; i++);
+            if (i >= m) {
+                match[j] = '1';
+                matchCount++;
+            }
+        }
+
+
+        for (int i = 0; i < matchLength; i++) {
+            if (match[i] == '1') System.out.print(i+" ");
+        }
+        System.out.println();
+        System.out.println("Total matches " + matchCount);
     }
 }
